45404c5e02ff90b58340890d5b7dcfc4183bb592
joshmaglione <joshmaglione@gmail.com>
2019-01-25 03:35:44 -0500

    Install & Update files

---
+++ install.sh
@@ -0,0 +1,69 @@
+# Vars
+FILE="$(realpath -s "$0")"              # Full path to this file
+DIR="$(dirname $FILE)"                  # Current directory
+PKGDIR="$(dirname $DIR)"                # Directory for dependencies
+START="${HOME}/.magmarc"                # Magma start file location
+
+# Dependencies and .spec locations
+ATTACH1="AttachSpec(\"$DIR/Densor.spec\");"
+ATTACH2="AttachSpec(\"$PKGDIR/CSS/CSS.spec\");"
+ATTACH3="AttachSpec(\"$PKGDIR/TensorSpace/TensorSpace.spec\");"
+
+
+echo "Densor.spec is in $DIR"
+
+
+echo "Dependencies will be downloaded to $PKGDIR"
+
+
+# CSS install/ update
+if [ -f "$PKGDIR/CSS/update.sh" ]
+then
+    echo "CSS already installed, updating..."
+    sh "$PKGDIR/CSS/update.sh"
+else
+    echo "Could not find CSS, downloading..."
+    cd "$PKGDIR"
+    git -q clone https://github.com/algeboy/CSS
+fi
+
+
+# TensorSpace install/ update
+if [ -f "$PKGDIR/TensorSpace/update.sh" ]
+then
+    echo "TensorSpace already installed, updating..."
+    sh "$PKGDIR/TensorSpace/update.sh"
+else
+    echo "Could not find TensorSpace, downloading..."
+    cd "$PKGDIR"
+    git -q clone https://github.com/algeboy/TensorSpace
+fi
+
+echo "Dependencies downloaded."
+
+
+
+
+# Construct Magma start file
+
+if [ -f "$START" ]
+then
+    echo "Found a Magma start file"
+    for A in "$ATTACH" "$ATTACH2" "$ATTACH3" do
+        if grep -Fxq "$A" "$START"
+        then
+            echo "Already installed"
+        else
+            echo "$A" >> "$START"
+            echo "Successfully installed"
+        fi
+    done
+else
+    echo "Creating a Magma start file: $START"
+    echo "// Created by an install file for Magma start up." > "$START"
+    for A in "$ATTACH" "$ATTACH2" "$ATTACH3" do
+        echo "$A" >> "$START"
+    done
+    echo "Successfully installed"
+fi
+
---
+++ update.sh
@@ -0,0 +1,44 @@
+# Maybe we can make this git independent or safer?
+# Upgrade redundancies likely exist. Shortcuts are purposefully avoided.
+
+# Vars
+FILE="$(realpath -s "$0")"
+DIR="$(dirname $FILE)"
+PKGDIR="$(dirname $DIR)"
+
+
+cd "$DIR" 
+
+# Update main package
+echo "Updating Densor package."
+git -q pull origin master 
+
+echo "Now updating dependencies."
+
+
+# CSS update
+if [ -f "$PKGDIR/CSS/update.sh" ]
+then
+    sh "$PKGDIR/CSS/update.sh"
+else
+    echo "Could not find CSS, downloading..."
+    cd "$PKGDIR"
+    git -q clone https://github.com/algeboy/CSS
+    echo "Installing CSS..."
+    sh "$PKGDIR/CSS/install.sh"
+fi
+
+# TensorSpace update
+if [ -f "$PKGDIR/TensorSpace/update.sh" ]
+then
+    sh "$PKGDIR/TensorSpace/update.sh"
+else
+    echo "Could not find TensorSpace, downloading..."
+    cd "$PKGDIR"
+    git -q clone https://github.com/algeboy/TensorSpace
+    echo "Installing TensorSpace..."
+    sh "$PKGDIR/TensorSpace/install.sh"
+fi
+
+echo "Successfully updated!"
+
